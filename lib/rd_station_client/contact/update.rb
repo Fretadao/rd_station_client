9da781d6f718e9caec6b90b9f28d56615176ea1f
Bruno Vicenzo <bruno@vicenzo.me>
2023-03-01 17:25:32 -0300

    Add tests to update contact

--- lib/rd_station_client/contact/update.rb
+++ lib/rd_station_client/contact/update.rb
@@ -6,13 +6,27 @@ module RDStationClient
     # Ex:
     #   RDStationClient::Contact::Update.(
     #     path_params: { identifier_type: 'email', identifier_value: 'contact@email.com' },
-    #     body: {
-    #       name: 'John Doe',
-    #       website: 'http://mysite.com',
-    #       bio: 'My name is John Doe'
-    #     }
+    #     body: { name: 'John Doe', website: 'http://mysite.com', bio: 'My name is John Doe' }
+    #   )
+    #
+    #   RDStationClient::Contact::Update.(
+    #     path_params: { identifier_type: 'uuid', identifier_value: 'adf6d3d6-31da-41bd-8e5c-b3fbfbafa8a8' }
+    #     body: { name: 'John Doe', website: 'http://mysite.com', bio: 'My name is John Doe' }
+    #   )
+    #
+    #   RDStationClient::Contact::Update.(
+    #     email: 'contact@email.com'
+    #     body: { name: 'John Doe', website: 'http://mysite.com', bio: 'My name is John Doe' }
+    #   )
+    #
+    #   RDStationClient::Contact::Update.(
+    #     uuid: 'adf6d3d6-31da-41bd-8e5c-b3fbfbafa8a8',
+    #     body: { name: 'John Doe', website: 'http://mysite.com', bio: 'My name is John Doe' }
     #   )
     class Update < RDStationClient::Authenticated
+      option :uuid, default: -> {}
+      option :email, default: -> {}
+
       private
 
       def make_request
@@ -21,6 +35,16 @@ module RDStationClient
 
       def path_template
         '/platform/contacts/%<identifier_type>s:%<identifier_value>s'
+      end
+
+      def path_params
+        if email.present?
+          { identifier_type: :email, identifier_value: email }
+        elsif uuid.present?
+          { identifier_type: :uuid, identifier_value: uuid }
+        else
+          super
+        end
       end
     end
   end
