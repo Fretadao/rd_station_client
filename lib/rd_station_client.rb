73eb0b73c4f293d1bb59b561434fdb728d7b3d48
Bruno Vicenzo <bruno.vicenzo@fretadao.com.br>
2023-03-01 09:29:17 -0300

    Add requirements

--- lib/rd_station_client.rb
+++ lib/rd_station_client.rb
@@ -3,11 +3,13 @@
 require 'f_http_client'
 require_relative 'rd_station_client/version'
 require 'rd_station_client/configuration'
+require 'rd_station_client/base'
+require 'rd_station_client/access_token/generate'
+require 'rd_station_client/access_token/storage'
+require 'rd_station_client/access_token/refresh'
+require 'rd_station_client/authenticated'
+require 'rd_station_client/contact/find'
+require 'rd_station_client/contact/update'
 
 module RDStationClient
-  extend FHTTPClient
-
-  def self.configuration_class
-    'RDStationClient::Configuration'
-  end
 end
